88d02ff24bd6538fbcb26710fc9a824427725baf
suelen <suelen.pesilva@gmail.com>
2023-08-10 12:31:40 -0400

    atividade respondida

---
+++ atividade01.py
@@ -0,0 +1,45 @@
+#1
+num1 = 10
+num2 = 5
+print(num1,num2)
+
+#2
+# classe complexa.
+
+#3
+#dicionários e conjuntos. O primeiro guarda valores por chave-valores e o segundo é uma lista de lementos que nao permite duplicatas em seus valores.
+
+#4
+# valido
+# valido
+# valido
+# invalido
+# valido
+# invalido
+
+#5
+#válido. Cada variavel retornou seu valor correspondente.
+#inválido. Esperava mais argumentos.
+#válido.
+
+#6
+#16
+# -26
+#9
+#8.5
+
+#7
+#  para caso seja necessário instalar pacotes ou realizar alterações especificas, as mudanças fiquem somente nesse ambiente, não necessáriamente levando as alterações para outros projetos.
+
+#8
+# python --version
+# Python 3.11.4
+
+#9
+#Não podemos atribuir informações aos  elementos da tupla separadamente,uma vez ela declarada. O que pode ser feito caso seja necessário é reescrever a tupla raiz, coma informação alterada.
+
+#10
+
+
+#11 Revise o código disponibilizado em src/primeiro.py. Em seguida altere o programa para que ele se torne generalista, ou seja, aceite qualquer quantidade de notas que cada aluno possa ter.
+
